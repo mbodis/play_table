e57550a2a34c650f46faaed4f367cc6bf4445bcc
Miroslav Bodis <miroslav.bodis@gmail.com>
2016-12-04 17:46:34 +0100

    [*] #132 disable dynamic on selected

--- play_table_master/libs/Sensor.h
+++ play_table_master/libs/Sensor.h
@@ -8,90 +8,100 @@
  */
 class Sensor {
 	private:
-  		byte sensorId = -1;
-    	byte mode = -1;
-    	byte channel = 0;
-    	byte volume = 0;
-    	bool autoMode = false;
-  		bool loopingIsOn = false;
-    
-  	public:
-        
-        Sensor(byte mId, byte mMode, byte mChannel){
-        	sensorId = mId;
-        	mode = mMode;
-        	channel = mChannel;
-        }
+		byte sensorId = -1;
+		byte mode = -1;
+		byte channel = 0;
+		byte volume = 0;
 
-        virtual void sensorOff(MyMidi&);
-        virtual void playNote(MyMidi&, byte, byte);
-        
-        byte getVolume(){
-        	return volume;
-        }
-        
-        void setVolume(byte mVolume){
-        	volume = mVolume;
-        }
+		// auto mode
+		bool autoMode = false;
+		bool loopingIsOn = false;
 
-        /*
-	     * toggle note setup helper for visualization
-	     */
-	    byte getSensorId(){
-	      	return sensorId;
-	    }
+		// dynamic mode
+		bool dynamicMode = false;
 
-	    byte visualLedsHelper = 0;
+	public:
 
-		void setSensorId(byte id){
+		Sensor(byte sensorId, byte mode, byte channel, bool mDynamicMode = false) {
+			this->sensorId = sensorId;
+			this->mode = mode;
+			this->channel = channel;
+			this->dynamicMode = mDynamicMode;
+		}
+
+		virtual void sensorOff(MyMidi&);
+		virtual void playNote(MyMidi&, byte, byte);
+
+		byte getVolume() {
+			return volume;
+		}
+
+		void setVolume(byte mVolume) {
+			volume = mVolume;
+		}
+
+		/*
+		 * toggle note setup helper for visualization
+		 */
+		byte getSensorId() {
+			return sensorId;
+		}
+
+		byte visualLedsHelper = 0;
+
+		void setSensorId(byte id) {
 			sensorId = id;
 		}
-	    
-	    byte getMode(){
+
+		byte getMode() {
 			return mode;
-	    }
-	      
-	    void setMode(byte newMode){
+		}
+
+		void setMode(byte newMode) {
 			mode = newMode;
-	    }
+		}
 
-	    byte getVisualLedHelper(){
-	    	return visualLedsHelper;
-	    }
-	    
-	    void setVisualLedHelper(byte vh){
-	    	visualLedsHelper = vh;
-	    }
+		byte getVisualLedHelper() {
+			return visualLedsHelper;
+		}
 
-	    byte getChannel(){
-	    	return channel;
-	    }
+		void setVisualLedHelper(byte vh) {
+			visualLedsHelper = vh;
+		}
 
-	    /*
-	     * toggle note setup helper for visualization method
-	     */
-        void incVisualLedHelper(byte inc){
+		byte getChannel() {
+			return channel;
+		}
+
+		/*
+		 * toggle note setup helper for visualization method
+		 */
+		void incVisualLedHelper(byte inc) {
 			visualLedsHelper += inc;
-			if (visualLedsHelper > 255){
+			if (visualLedsHelper > 255) {
 				visualLedsHelper = 255;
 			}
-	    }
+		}
 
-	    bool hasAutoMode(){
-	    	return autoMode;
-	    }
+		bool hasAutoMode() {
+			return autoMode;
+		}
 
-	    void setAutoMode(bool loopModeVal){
-	    	autoMode = loopModeVal;
-	    }
+		void setAutoMode(bool loopModeVal) {
+			autoMode = loopModeVal;
+		}
 
-	    bool isLoopingEnabled(){
-	    	return loopingIsOn;
-	    }
+		bool isLoopingEnabled() {
+			return loopingIsOn;
+		}
 
-	    void setLooping(bool loopState){
-	    	loopingIsOn = loopState;
-	    }
+		void setLooping(bool loopState) {
+			loopingIsOn = loopState;
+		}
+
+		bool isDynamicModeEnable() {
+			return dynamicMode;
+		}
 };
 
 
